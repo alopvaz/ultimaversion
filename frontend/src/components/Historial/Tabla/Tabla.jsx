26bbdc7e488c24ff6959325acdf2cf3860383034
alopvaz014 <alopvaz014@iesalmudeyne.es>
2024-01-18 20:43:36 +0100

    tabla final

--- frontend/src/components/Historial/Tabla/Tabla.jsx
+++ frontend/src/components/Historial/Tabla/Tabla.jsx
@@ -1,241 +1,194 @@
-import React from 'react';
-import { Table, Input, Button, Space, Dropdown, Menu } from 'antd';
-import { useState, useEffect } from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React, { useState } from 'react';
+import { Row, Col, Button, Input, Table, Space, Typography, Popconfirm, Form } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import './Tabla.css';
 
-const { Column } = Table;
+const Tabla = () => {
 
-const Tabla = ({
-  title,
-  data = [],
-  columns,
-  onEditClick,
-  onDeleteClick,
-  onViewClick,
-  onInputChange,
-}) => {
-  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const [editableData, setEditableData] = useState(data);
-  const [filteredData, setFilteredData] = useState(editableData);
-  const [editingCell, setEditingCell] = useState({ key: '', column: '' });
 
-  const onRowClick = (record, rowIndex) => {
-    setSelectedRowKeys([record.key]);
+
+
+  const [inputVisible, setInputVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [data, setData] = useState([
+    { key: '1', name: 'John Doe', age: 30, address: 'New York' },
+    { key: '2', name: 'John Doe', age: 30, address: 'New York' },
+    { key: '3', name: 'John Doe', age: 30, address: 'New York' },
+    { key: '4', name: 'John Doe', age: 30, address: 'New York' },
+  ]);
+  const [editingKey, setEditingKey] = useState('');
+
+  const handleClick = () => {
+    setInputVisible(!inputVisible);
   };
 
-  const rowSelection = {
-    selectedRowKeys,
-    onChange: (selectedRowKeys, selectedRows) => {
-      setSelectedRowKeys(selectedRowKeys);
-    },
+  const isEditing = (record) => record.key === editingKey;
+
+  const edit = (record) => {
+    form.setFieldsValue(record);
+    setEditingKey(record.key);
+  };
+  const cancel = () => {
+    setEditingKey('');
   };
 
-  const [searchVisible, setSearchVisible] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-    const filteredData = editableData.filter((row) =>
-      columns.some((column) =>
-        row[column.dataIndex]
-          .toString()
-          .toLowerCase()
-          .includes(e.target.value.toLowerCase())
-      )
-    );
-    setFilteredData(filteredData);
-  };
-
-  const handleInputChange = (e, key, column) => {
-    const newData = [...editableData];
-    const target = newData.find((item) => item.key === key);
-
-    if (target) {
-      target[column] = e.target.value;
-      setEditableData(newData);
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
     }
   };
 
-  const editableColumns = columns.map((column) => ({
-    ...column,
-    render: (text, record) => {
-      const isEditing =
-        record.key === editingCell.key &&
-        column.dataIndex === editingCell.column;
+  const handleKeyPress = (e, key) => {
+    if (e.key === 'Enter') {
+      save(key);
+    }
+  };
 
-      return isEditing ? (
-        <Input
-          className='borderless-input'
-          value={text}
-          onChange={(e) => handleInputChange(e, record.key, column.dataIndex)}
-          style={{ width: '100%', height: '100%' }}
-        />
-      ) : (
-        <div
-          onClick={(e) => {
-            e.stopPropagation();
-            setEditingCell({
-              key: record.key,
-              column: column.dataIndex,
-            });
-          }}
-          style={{ cursor: 'pointer' }}
-        >
-          {text}
-        </div>
-      );
+  const handleAdd = () => {
+    const newKey = (data.length + 1).toString();
+    const newData = {
+      key: newKey,
+      name: `New User ${newKey}`,
+      age: '',
+      address: '',
+    };
+    setData([...data, newData]);
+    edit(newData);
+  };
+
+  const handleDelete = (key) => {
+    const newData = data.filter((item) => item.key !== key);
+    setData(newData);
+  };
+
+  const columns = [
+    {
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      sortDirections: ['descend', 'ascend'],
+      render: (text, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <Form.Item
+            name="name"
+            style={{
+              margin: 0,
+            }}
+            rules={[
+              {
+                required: true,
+                message: 'Please input a name!',
+              },
+            ]}
+          >
+            <Input
+              onBlur={() => save(record.key)}
+              onPressEnter={(e) => handleKeyPress(e, record.key)}
+            />
+          </Form.Item>
+        ) : (
+          <span>{text}</span>
+        );
+      },
     },
-  }));
+    {
+      title: 'acciones',
+      key: 'expand',
+      render: (text, record) => {
+        const editable = isEditing(record);
+        return (
+          <span>
+            <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}       style={{ marginRight: '10px' }}
+>
+              Editar 
+            </Typography.Link>
+            {" "} {" "}
 
-  useEffect(() => {
-    setFilteredData(
-      editableData.filter((row) =>
-        columns.some((column) =>
-          row[column.dataIndex]
-            .toString()
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase())
-        )
-      )
-    );
-  }, [editableData, searchTerm]);
+            <Typography.Link onClick={() => handleDelete(record.key)}>
+              Eliminar
+            </Typography.Link>
+          </span>
+        );
+      },
+    },
+  ];
+
+  const [searchValue, setSearchValue] = useState('');
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   return (
-    <div className='main'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12'>
-            <h2 className='card-title'>{title}</h2>
-          </div>
+    <div className="main">
+      <div className="container">
+        <div className="title">
+          <h2 className="t">TABLA DE SESIONES</h2>
         </div>
-        <div className='row actions'>
-          <div className='col-6 left-actions'>
-            {selectedRowKeys.length === 1 && (
-              <>
-                <Button className='icon-button' onClick={onViewClick}>
-                  <i className='bi bi-eye'></i>
+
+        <div className="actions">
+          <Row gutter={[16, 16]}>
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+              <div className="left-actions">
+                <Button type="primary" onClick={handleAdd}>
+                  Agregar Fila
                 </Button>
-                <Button className='icon-button' onClick={onEditClick}>
-                  <i className='bi bi-pencil'></i>
-                </Button>
-              </>
-            )}
-            {selectedRowKeys.length >= 1 && (
-              <Button className='icon-button' onClick={onDeleteClick}>
-                <i className='bi bi-trash'></i>
-              </Button>
-            )}
-          </div>
-          <div className='col-6 right-actions'>
-            <Space className='space'>
-              <i
-                className='bi bi-search'
-                onClick={() => setSearchVisible(!searchVisible)}
-              ></i>
-              {searchVisible && (
-                <Input
-                  className='input-buscar'
-                  placeholder='Buscar...'
-                  onChange={handleSearchChange}
-                  value={searchTerm}
-                />
-              )}
-            </Space>
-          </div>
-        </div>
-        <div className='row table-container'>
-          <div className='col-12'>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+  <div className="right-actions">
+    <Button onClick={handleClick}>
+      <SearchOutlined />
+    </Button>
+    {inputVisible && (
+      <Input
+        className="input-visible"
+        placeholder="Buscar..."
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+    )}
+  </div>
+</Col>
+          </Row>
+        </div> 
+
+        <div className="table">
+          <Form form={form} component={false}>
             <Table
-              className='table table-dark table-sm'
-              rowSelection={rowSelection}
+              columns={columns}
               dataSource={filteredData}
-              pagination={{
-                pageSize: 8,
-                position: ['bottomCenter'],
-                showSizeChanger: false,
-                showQuickJumper: false,
-                showTotal: false,
-                simple: true,
-              }}
-              size='small'
-            >
-              {editableColumns.map((column) => (
-                <Column {...column} />
-              ))}
-              {/* Añadir columna de menú desplegable */}
-              <Column
-                title='Acciones'
-                key='actions'
-                render={(text, record) => (
-                  <Dropdown
-                    overlay={
-                      <Menu>
-                        <Menu.Item key='edit' onClick={() => onEditClick(record)}>
-                          Editar
-                        </Menu.Item>
-                        <Menu.Item
-                          key='delete'
-                          onClick={() => onDeleteClick(record)}
-                        >
-                          Eliminar
-                        </Menu.Item>
-                      </Menu>
-                    }
-                    trigger={['hover']}
-                  >
-                    <a className='ant-dropdown-link' onClick={(e) => e.preventDefault()}>
-                      <i className='bi bi-three-dots-vertical'></i>
-                    </a>
-                  </Dropdown>
-                )}
-              />
-            </Table>
-          </div>
+              expandable={{ expandedRowRender: () => null }}
+            />
+          </Form>
         </div>
       </div>
     </div>
   );
-};
-
-
-
-Tabla.propTypes = {
-  title: PropTypes.string,
-  data: PropTypes.array,
-  columns: PropTypes.array,
-  onEditClick: PropTypes.func,
-  onDeleteClick: PropTypes.func,
-  onViewClick: PropTypes.func,
-};
-
-Tabla.defaultProps = {
-  title: "Mi Tabla",
-  data: [
-    { key: 1, name: 'John Doe', age: 32, address: 'Street 1' },
-    { key: 2, name: 'Jane Doe', age: 28, address: 'Street 2' },
-    { key: 3, name: 'Jim Doe', age: 42, address: 'Street 3' },
-    { key: 4, name: 'John Doe', age: 32, address: 'Street 1' },
-    { key: 5, name: 'Jane Doe', age: 28, address: 'Street 2' },
-    { key: 6, name: 'Jim Doe', age: 42, address: 'Street 3' },
-    { key: 7, name: 'John Doe', age: 32, address: 'Street 1' },
-    { key: 8, name: 'Jane Doe', age: 28, address: 'Street 2' },
-    { key: 9, name: 'Jim Doe', age: 42, address: 'Street 3' },
-    { key: 10, name: 'John Doe', age: 32, address: 'Street 1' },
-    { key: 11, name: 'Jane Doe', age: 28, address: 'Street 2' },
-    { key: 12, name: 'Jim Doe', age: 42, address: 'Street 3' },
-  ],
-    columns: [
-      {
-        title: 'Nombre',
-        dataIndex: 'name',
-        key: 'name',
-      },   
-      { title: 'Age', dataIndex: 'age', key: 'age' },
-      { title: 'Address', dataIndex: 'address', key: 'address' },
-    ],
-  onEditClick: () => console.log('Edit clicked'),
-  onDeleteClick: () => console.log('Delete clicked'),
-  onViewClick: () => console.log('View clicked'),
 };
 
 export default Tabla;
